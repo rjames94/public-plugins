188b25a5a9ef7323cbe86c7a0b6c671a132ab4e2
lucid-soft <lucid-soft@github.com>
2024-04-15 22:22:30 -0400

    Lucid Hotkeys 1.2.9 -
    Changelist:
    - Added new Action SUBTRACT_VALUE_FROM_VARIABLE which will subtract a given amount from a defined variable (only if the variable value is a number)
    - Added new Action EVALUATE_HOTKEY_IN_X_TICKS which lets you evaluate (and execute if its valid) a hotkey in x amount of ticks
    - Created a new config section called "General" and moved the "Use as Bot" option to it
    - Added a new option for debugging hotkeys which will tell you if a hotkey succeeded it's preconditions and if not, which one failed. This is added to General settings
    - Fixed the logic for SET_VAR_VALUE, VAR_VALUE_EQUALS, VAR_VALUE_NOT_EQUAL, ADD_VALUE_TO_VARIABLE and other variable-related actions that had wonky logic
    - You can now remove/set the delay even if there is an ongoing delay

--- lucid-hotkeys/lucid-hotkeys.gradle.kts
+++ lucid-hotkeys/lucid-hotkeys.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.2.8"
+version = "1.2.9"
 
 project.extra["PluginName"] = "Lucid Hotkeys"
 project.extra["PluginDescription"] = "Setup hotkeys that can do a variety of different actions."
